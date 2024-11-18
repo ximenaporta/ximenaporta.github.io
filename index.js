19f7392e54a54c043be414a7e8c7f4bad3335a57
LachlanNewman <lachnewman007@gmail.com>
2024-11-18 11:37:36 +1100

    scoll through portfolio

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ var { pdfjsLib } = globalThis;
 // The workerSrc property shall be specified.
 pdfjsLib.GlobalWorkerOptions.workerSrc = './pdf.worker.mjs';
 
-const canvases = [
-    document.getElementById('canvas-1'),
-]
-
-const contexts = canvases.map(canvas => canvas.getContext('2d'))
-
 var pdfDoc = null,
     pageNum = 1,
     pageRendering = false,
@@ -29,7 +23,7 @@ function getRenderContext(page,canvas, ctx) {
 
     // Render PDF page into canvas context
     return {
-        canvasContext: ctx,
+        canvasContext: canvas.getContext('2d'),
         viewport: viewport
     };
 }
@@ -38,68 +32,29 @@ function getRenderContext(page,canvas, ctx) {
  * Get page info from document, resize canvas accordingly, and render page.
  * @param num Page number.
  */
-function renderPage(num) {
-    pageRendering = true;
-    const pageNums = Array.from({length: canvases.length}, (_, i) => i + num)
-    console.log(pageNums)
+function renderPage(num, canvas) {
     // Using promise to fetch the page
-    Promise.all(pageNums.map(pageNum => pdfDoc.getPage(pageNum))).then((pages) => {
-    const renderTasks = pages.map((page,i) => ({page, renderContext: getRenderContext(page,canvases[i],contexts[i])}))
-    .map(({page, renderContext}) => page.render(renderContext).promise)
-
-
-    Promise.all(renderTasks).then(() => {
-        pageRendering = false;
-        if (pageNumPending !== null) {
-        // New page rendering is pending
-        renderPage(pageNumPending);
-        pageNumPending = null;
-        }
-    })
+    pdfDoc.getPage(num).then((page) => {
+        var renderContext = getRenderContext(page,canvas)
+        page.render(renderContext)
     })
 }
 
-/**
- * If another page rendering in progress, waits until the rendering is
- * finised. Otherwise, executes rendering immediately.
- */
-function queueRenderPage(num) {
-    if (pageRendering) {
-    pageNumPending = num;
-    } else {
-    renderPage(num);
-    }
-}
 
-/**
- * Displays previous page.
- */
-function onPrevPage() {
-    if (pageNum <= 1) {
-    return;
-    }
-    pageNum = pageNum - 1;
-    queueRenderPage(pageNum);
-}
-document.getElementById('prev').addEventListener('click', onPrevPage);
-
-/**
- * Displays next page.
- */
-function onNextPage() {
-    if (pageNum >= pdfDoc.numPages) {
-    return;
-    }
-    pageNum = pageNum + 1;
-    queueRenderPage(pageNum);
-}
-document.getElementById('next').addEventListener('click', onNextPage);
 
 /**
  * Asynchronously downloads PDF.
  */
 pdfjsLib.getDocument(url).promise.then(function(pdfDoc_) {
     pdfDoc = pdfDoc_;
+    var viewer = document.getElementById('pdf-viewer');
+    
+    var page
+    for(page = 1; page <= pdfDoc.numPages; page++) {
+      var canvas = document.createElement("canvas");    
+      viewer.appendChild(canvas);            
+      renderPage(page, canvas);
+    }
 
     // Initial/first page rendering
     renderPage(pageNum);
